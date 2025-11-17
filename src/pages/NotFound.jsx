c6d3ae3049215a02b1660b3f1d06b2a44bf14e27
kosterseb <sebkos.code@gmail.com>
2025-11-17 12:05:50 +0100

    Feat: Added some PageNotFound 404 for the app Edit: made some styling for the tutorial

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,9 +3,11 @@ import './NotFound.sass';
 
 export default function NotFound() {
     return (
+        <>
         <div className="not-found-page">
             <h1>404 - Page Not Found</h1>
             <p>Sorry, the page you are looking for does not exist.</p>
         </div>
+        </>
     );
 }
--- src/pages/Tutorial.jsx
+++ src/pages/Tutorial.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import './Tutorial.sass';
 
-const BackgroundImage = '../../assignment_assets/background.svg';
+const BackgroundImage = '../../assignment_assets/Animal.svg';
 
 export default function Tutorial() {
     return (
+        <>
         <div className="tutorial-page">
             <img src={BackgroundImage} alt="Background" className="background-image" />
             <div className="tutorial-content">
-                <h1>Welcome to the Tutorial Page for Mets!</h1>
+                <h1>Welcome to the Tutorial Page for METS!</h1>
                 <p>Get ready to look for some great pets out there!</p>
             </div>
         </div>
+        <div className="next-button">
+                <button>Next</button>
+            </div>
+        </>
     );
 }
