3631e09902c9d56f32412aa86cc7cda33597afba
kosterseb <sebkos.code@gmail.com>
2025-11-17 09:30:30 +0100

    Feat: added the homepage setup and the components im gonna need. Note: Im Gonna add a extra page called tutorial for the assignment

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Navbar from "../components/Universal/Navbar";
+import AnimalList from "../components/HomePage/AnimalList";
+import Category from "../components/HomePage/Category";
+import Location from "../components/HomePage/Location";
+import MiniProfileMenu from "../components/HomePage/MiniProfileMenu";
+import Notifications from "../components/HomePage/Notifications";
+import './Home.sass';
+
+export default function Home() {
+    return (
+        <div className="home-page">
+            <Navbar />
+            <div className="home-content">
+                <aside className="home-sidebar-left">
+                    <MiniProfileMenu />
+                    <Category />
+                    <Location />
+                </aside>
+                <main className="home-main-content">
+                    <AnimalList />
+                </main>
+                <aside className="home-sidebar-right">
+                    <Notifications />
+                </aside>
+            </div>
+        </div>
+    );
+}
