f000bb2f0dcb7546f8326b5c6ea63c8cc794d994
kosterseb <sebkos.code@gmail.com>
2025-11-17 13:14:21 +0100

    Feat: Made the button work in the tutorial to move on to the home page

--- src/pages/Tutorial.jsx
+++ src/pages/Tutorial.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import './Tutorial.sass';
+import { useNavigate } from 'react-router';
 
 const BackgroundImage = '../../assignment_assets/Animal.svg';
 
+
+
 export default function Tutorial() {
+    const navigate = useNavigate();
+
+    const navigateToNextPage = () => {
+        navigate('/home');
+    };
+
     return (
         <>
-        <div className="tutorial-page">
-            <img src={BackgroundImage} alt="Background" className="background-image" />
-            <div className="tutorial-content">
-                <h1>Welcome to the Tutorial Page for METS!</h1>
-                <p>Get ready to look for some great pets out there!</p>
+            <div className="tutorial-page">
+                <img src={BackgroundImage} alt="Background" className="background-image" />
+                <div className="tutorial-content">
+                    <h1>Welcome to the Tutorial Page for METS!</h1>
+                    <p>Get ready to look for some great pets out there!</p>
+                </div>
             </div>
-        </div>
-        <div className="next-button">
-                <button>Next</button>
+            <div className="next-button">
+                <button onClick={navigateToNextPage}>Next</button>
             </div>
         </>
     );
